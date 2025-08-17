a2e2e8241b9377d830dc5da822597770362cafa8
João Messias <joaogmessias@gmail.com>
2025-08-16 22:30:10 -0300

    Commit Terraform + CICD sem diretorio .terraform

--- .terraform/infra/main.tf
+++
@@ -1,9 +0,0 @@
-module "ec2" {
-  source = "./modules/ec2"
-
-  instance_name = var.ec2_instance_name
-  instance_type = var.ec2_instance_type
-  key_name      = var.ec2_key_name
-  region        = var.aws_region
-}
-
--- .terraform/infra/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-aws_region        = "sa-east-1"
-ec2_key_name      = "chave-teste"   
-ec2_instance_name = "vm-ubuntu-teste-node"
