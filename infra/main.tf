a3f0249d6af713042ea91bebb7bd2811cebda3be
João Messias <joaogmessias@gmail.com>
2025-08-24 19:32:02 -0300

    S3 Replication 12

--- infra/main.tf
+++ infra/main.tf
@@ -10,8 +10,8 @@ module "ec2" {
 module "s3_bucket_crr" {
   source              = "./modules/s3_bucket_crr"
 
-  source_bucket_name  = "terraform-techdev"
-  replica_bucket_name = "terraform-techdev-replica"
+  source_bucket_name  = "terraform-techdev2"
+  replica_bucket_name = "terraform-techdev2-replica"
   source_region       = "sa-east-1"
   replica_region      = "us-east-1"
 }
